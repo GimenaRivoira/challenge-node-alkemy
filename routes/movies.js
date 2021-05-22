023fd6af23e38f808a015ec5c8610ca845af1e24
GimenaRivoira <gimenarivoira@gmail.com>
2021-05-22 19:13:50 -0300

    verifyToken finished

--- routes/movies.js
+++ routes/movies.js
@@ -4,10 +4,12 @@ var router = express.Router();
 
 const {getAll, getOne, create, edit, remove} = require('../API/moviesController');
 
+const verifyToken = require('../middleware/authMiddleware');
+
 router.get('/', getAll);
 router.get('/:id', getOne);
-router.post('/create', create);
-router.put('/edit/:id', edit);
-router.delete('/delete/:id', remove);
+router.post('/create',verifyToken, create);
+router.put('/edit/:id',verifyToken, edit);
+router.delete('/delete/:id',verifyToken, remove);
 
 module.exports = router;
